000fd806f633e109b430abdd37b885ed60403a11
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-11-06 23:14:00 +0530

    connecting to MongoDB database

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
 
+console.log(process.env.MONGO_URL);
 const port = process.env.PORT || 3000;
-app.listen(port, ()=>{
-    console.log(`server listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
 });
