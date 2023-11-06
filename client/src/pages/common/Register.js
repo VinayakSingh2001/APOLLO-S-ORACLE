992e3ae70063e4ac64de88cf8c4b051d588296e4
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-11-06 16:09:33 +0530

    seting up server

--- client/src/pages/common/Register.js
+++ client/src/pages/common/Register.js
@@ -3,16 +3,27 @@ import { Link } from "react-router-dom";
 
 const Register = () => {
   const [rememberMe, setRememberMe] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
   const handleRememberMeChange = () => {
     setRememberMe(!rememberMe);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Username : ", username);
+    console.log("Password : ", password);
+    console.log("Email : ", email);
+    // console.log(values);
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 sm:p-12 md:p-16 rounded shadow-md sm:w-96 md:w-112 lg:w-128 xl:w-144">
         <h1 className="text-2xl font-semibold mb-4 text-center">Register</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block text-gray-600 text-sm font-medium">
               Username
@@ -21,6 +32,8 @@ const Register = () => {
               type="text"
               className="w-full border p-2 rounded focus:outline-none focus:ring focus:border-blue-300"
               placeholder="Enter your username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div className="mb-4">
@@ -31,6 +44,8 @@ const Register = () => {
               type="email"
               className="w-full border p-2 rounded focus:outline-none focus:ring focus:border-blue-300"
               placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-4">
@@ -41,6 +56,8 @@ const Register = () => {
               type="password"
               className="w-full border p-2 rounded focus:outline-none focus:ring focus:border-blue-300"
               placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className="mb-4">
