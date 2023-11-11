f706842e0e249e46e6a45d56e252be30bf9ce392
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-11-11 23:52:24 +0530

    dashboard added

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/common/Home";
 import Login from "./pages/common/Login";
 import Signup from "./pages/common/Signup";
+import Dashboard from "./pages/common/Dashboard";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
