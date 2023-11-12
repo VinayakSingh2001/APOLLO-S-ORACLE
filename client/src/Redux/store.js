72f4648c5861414c574a4b973333d908c4f12a13
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-11-12 23:24:22 +0530

    adding Authorization

---
+++ client/src/Redux/store.js
@@ -0,0 +1,10 @@
+import usersSlice from "./userSlice";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    users: usersSlice,
+  },
+});
+
+export default store;
---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProtectedRoute = ({children}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default ProtectedRoute
