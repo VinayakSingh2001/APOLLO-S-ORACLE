fd29db183cf1e78c57e91e92775c80a702dd6e70
Vinayak Singh <97396725+VinayakSingh2001@users.noreply.github.com>
2023-11-16 13:23:47 +0530

    setting up HTTP requests for Exam features

--- client/src/apicalls/exams.js
+++ client/src/apicalls/exams.js
@@ -10,3 +10,22 @@ export const addExam = async (payload) => {
     return error.response.data;
   }
 };
+
+//get all exams
+export const getAllExams = async () => {
+    try {
+        const response = await axiosInstance.post("/api/exams/get-all-exams");
+        return response.data;
+      } catch (error) {
+        return error.response.data;
+      }
+}
+//get exam by id
+export const getExamById = async (payload) => {
+    try {
+        const response = await axiosInstance.post("/api/exams/get-exam-by-id", payload);
+        return response.data;
+      } catch (error) {
+        return error.response.data;
+      }
+}
