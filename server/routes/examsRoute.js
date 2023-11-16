5f0980aac1d72a14241fe5e80b5ac409db58db35
Vinayak Singh <97396725+VinayakSingh2001@users.noreply.github.com>
2023-11-16 13:08:03 +0530

    API endpoints for handling exam-related operations

--- server/routes/examsRoute.js
+++ server/routes/examsRoute.js
@@ -29,4 +29,40 @@ router.post("/add", authMiddleware, async (req, res) => {
   }
 });
 
+//get all exams
+router.post("/get-all-exams", authMiddleware, async (req, res) => {
+  try {
+    const exams = await Exam.find({});
+    res.send({
+      message: "Exams fetched successfully",
+      data: exams,
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+      data: error,
+      success: false,
+    });
+  }
+});
+//get exam by id
+router.post("/get-exam-by-id", authMiddleware, async (req, res) => {
+  try {
+    const exam = await Exam.findById(req.body.examId);
+    res.send({
+      message: "Exam fetched successfully",
+      data: exam,
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+      data: error,
+      success: false,
+    });
+  }
+});
+
+
 module.exports = router;
